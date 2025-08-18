fcabc24a1502fd805c926f18629e9441d30e066e
loleik <lolei+github@lolei.uk>
2025-08-18 12:49:30 +0100

    Moved to sparse grid

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{thread, time};
 use clap::{arg, Command};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use regex::Regex;
 
 // Offsets used for checking neighbors.
 const OFFSETS: [(isize, isize); 8] = [
@@ -12,15 +13,25 @@ const OFFSETS: [(isize, isize); 8] = [
 // Struct for tracking the world. Uses double buffering to keep with the original rules.
 #[derive(Clone)]
 struct World {
-    width: usize,
-    height: usize,
-    current: Vec<bool>,
-    next: Vec<bool>
+    height: usize, width: usize, // Dimensions of the viewport specifically
+    current: HashSet<(isize, isize)>,
+    next: HashSet<(isize, isize)>,
+    neighbors: HashMap<(isize, isize), usize>
 }
 
 impl World {
-    fn new(width: usize, height: usize, map: Vec<bool>) -> World {
-        return World { width: width, height: height, current: map.clone(), next: map}
+    fn new(
+        height: usize, width: usize,
+        map1: HashSet<(isize, isize)>,
+        map2: HashSet<(isize, isize)>
+    ) -> World {
+        return World { 
+            height: height, 
+            width: width, 
+            current: map1, 
+            next: map2,
+            neighbors: HashMap::new()
+        }
     }
 }
 
@@ -32,72 +43,27 @@ fn cli() -> Command {
         .allow_external_subcommands(true)
         .subcommand(
             Command::new("pattern")
-                .about("Specify a starting pattern from [\"copperhead\"]")
-                .arg(arg!(<NAME> "The name of the pattern"))
+                .about("Specify a starting pattern to run")
+                .arg(arg!(<PATTERN> "Starting pattern e.g.[(0, 1),(1, 1),(2, 1)]"))
                 .arg_required_else_help(true),
         )
 }
 
-
-fn place_pattern(
-    map: &mut Vec<bool>,
-    map_width: usize,
-    pattern: &Vec<bool>,
-    pattern_width: usize,
-    pattern_height: usize,
-    offset: (usize, usize),
-) {
-    let (ox, oy) = offset;
-
-    for y in 0..pattern_height {
-        for x in 0..pattern_width {
-            let board_x = ox + x;
-            let board_y = oy + y;
-
-            // make sure we don't go out of bounds
-            if board_x < map_width && board_y < map.len() / map_width {
-                let board_idx = board_y * map_width + board_x;
-                let pattern_idx = y * pattern_width + x;
-                map[board_idx] = pattern[pattern_idx];
-            }
-        }
-    }
-}
-
 // Initialize a world structure using an initial pattern. Hard coded for now.
 fn init(
-    height: usize, 
+    height: usize,
     width: usize,
-    name: &str
-) -> World{
-    // Hardcoded example with the start co-ordinate.
-    let copperhead: (Vec<bool>, (usize, usize), usize, usize) = (vec![
-        false, false, false, false, false, true, false, true, true, false, false, false,
-        false, false, false, false, true, false, false, false, false, false, false, true,
-        false, false, false, true, true, false, false, false, true, false, false, true,
-        true, true, false, true, false, false, false, false, false, true, true, false,
-        true, true, false, true, false, false, false, false, false, true, true, false,
-        false, false, false, true, true, false, false, false, true, false, false, true,
-        false, false, false, false, true, false, false, false, false, false, false, true,
-        false, false, false, false, false, true, false, true, true, false, false, false,
-    ], (1, 6), 12, 8);
+    pattern: &str
+) -> World {    
+    let extract = Regex::new(r"\((\d+),(\d+)\)").unwrap();
 
-    let patterns: HashMap<&'static str, (Vec<bool>, (usize, usize), usize, usize)> = HashMap::from([
-        ("copperhead", copperhead)
-    ]);
+    let coords: HashSet<(isize, isize)> = extract.captures_iter(pattern)
+                    .map(|cap| {(
+                        cap[1].parse::<isize>().unwrap(),
+                        cap[2].parse::<isize>().unwrap(),
+                    )}).collect();
 
-    let mut initial: Vec<bool> = vec![false; width * height];
-
-    place_pattern(
-        &mut initial, 
-        width, 
-        &patterns.get(name).unwrap().0, 
-        patterns.get(name).unwrap().2, 
-        patterns.get(name).unwrap().3, 
-        patterns.get(name).unwrap().1
-    );
-
-    let world: World = World::new(width, height, initial);
+    let world: World = World::new(height, width, coords.clone(), coords);
 
     world
 }
@@ -108,8 +74,11 @@ fn view(
 ) {
     for y in 0..world.height {
         for x in 0..world.width {
-            let i = y * world.width + x;
-            print!("{}", if world.current[i] { " ■ " } else { " □ " })
+            if world.current.contains(&(x.try_into().unwrap(), y.try_into().unwrap())) {
+                print!("■ ");
+            } else {
+                print!("□ ");
+            }
         }
         println!();
     }
@@ -118,19 +87,17 @@ fn view(
 // Uses offset values to check how many neighbors each cell has.
 fn check_neighbors(
     world: &World, 
-    x: usize, 
-    y: usize
+    x: isize, 
+    y: isize
 ) -> usize {
     let mut count: usize = 0;
 
     for (ox, oy) in OFFSETS {
-        let cx: isize = x as isize + ox;
-        let cy: isize = y as isize + oy;
+        let cx: isize = x + ox;
+        let cy: isize = y + oy;
 
-        if cx >= 0 && cx < world.width as isize && cy >= 0 && cy < world.height as isize {
-            let index: usize = cy as usize * world.width + cx as usize;
-
-            if world.current[index] { count += 1 }
+        if world.current.contains(&(cx, cy)) {
+            count += 1
         }
     }
 
@@ -142,46 +109,64 @@ fn check_neighbors(
 fn tick(
     world: &mut World
 ) {
-    for i in 0..world.current.len() {
-        let x: usize = i % world.width;
-        let y: usize = i / world.width;
+    for &(x, y) in &world.current {
+        let count: usize = check_neighbors(&world, x, y);
 
-        let count: usize = check_neighbors(world, x, y);
+        for (ox, oy) in OFFSETS {
+            *world.neighbors.entry((x + ox, y + oy)).or_insert(0) += 1;
+        }
 
-        match (world.current[i], count) {
-            (true, 0) | (true, 1) => { world.next[i] = false }, // underpopulation
-            (true, 2) | (true, 3) => {}, // unchanged
-            (true, _) => { world.next[i] = false }, // overpopulation
-            (false, 3) => { world.next[i] = true }, // reproduction
-            (_, _) => {}
+        match count {
+            0 | 1 => { world.next.remove(&(x, y)); }, // underpopulation
+            2 | 3 => {}, // unchanged
+            _ => { world.next.remove(&(x, y)); } // overpopulation
+        }        
+    }
+
+    // Handle reproduction
+    for (&(x, y), &c) in &world.neighbors {
+        if c == 3 {
+            world.next.insert((x, y));
         }
     }
 
-    world.current.copy_from_slice(&world.next);
+    world.current = world.next.clone();
+    world.neighbors = HashMap::new();
 }
 
-fn main() {
+fn main() -> Result<(), ()> {
     let matches: clap::ArgMatches = cli().get_matches();
 
     match matches.subcommand() {
         Some(("pattern", sub_matches)) => {
-            let name: &String = sub_matches.get_one::<String>("NAME").expect("Required");
+            let pattern: &String = sub_matches.get_one::<String>("PATTERN")
+                    .expect("Required");
 
-            let mut world: World = init(20, 40, &name);
+            let validate = Regex::new(r"^\[\(\d+,\d+\)(?:,\(\d+,\d+\))*\]$").unwrap();
 
-            for i in 0..200 {
+            if !validate.is_match(pattern) {
+                println!("Please enter a coordinate list of the following form:");
+                println!("[(x1,y1),(x2,y2),...(xn,yn)]");
+                return Ok(())
+            }
+
+            let mut world: World = init(3, 3, &pattern);
+
+            for i in 0..50 {
                 print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
         
                 view(&world);
 
-                println!("Running: {name} | Tick: {i}");
-        
+                println!("Running: {pattern} | Tick: {i}");
+
                 tick(&mut world);
-        
+
                 let wait: time::Duration = time::Duration::from_millis(250);
                 thread::sleep(wait);
             }
-        }
-        _ => {},
+        },
+        _ => println!("Invalid subcommand: {}", matches.subcommand().unwrap().0)
     }
+
+    Ok(())
 }
